d14d732f1a059835e59773717e7104e678d73766
Shani Staretz AI <shani.staretz@gmail.com>
2026-02-24 21:38:04 +0200

    Refactor input field IDs, enhance button click functionality, and add new HTML/CSS files for student directory and follow-me sections

--- 12-01/12-01.js
+++ 12-01/12-01.js
@@ -4,9 +4,15 @@ In a later version, the user will be asked to fill their name in an input box,
 and the pop-up message will show the user’s name.
  */
 function onBlueButtonClick() {
+  console.log("blue button clicked");
   alert("You have clicked the Button!");
 }
 
 function onClickButton() {
-  alert("hello " + document.getElementById("name-input").value);
+  alert("hello " + document.getElementById("name-input").value); // Clicked!
+  console.log("hello shani");
+}
+
+function onClickButton1() {
+  alert("hello " + document.getElementById("name-input").value); // Clicked!
 }
